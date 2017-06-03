1a11610d2f26859499e5bd0348bfadf419271b1b
kun.lin <kun.lin@talebase.com>
2017-06-03 21:43:23 +0800

    添加项目

---
+++ FKLFM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FKLFM
+//
+//  Created by kun on 2017/6/3.
+//  Copyright © 2017年 kun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FKLFM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FKLFM
+//
+//  Created by kun on 2017/6/3.
+//  Copyright © 2017年 kun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
